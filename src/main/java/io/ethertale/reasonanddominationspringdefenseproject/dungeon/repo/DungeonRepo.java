965868e5cd506066fe0b97c7af988796e0c03499
Galin Yanakov <galin@ivel.in>
2025-03-01 12:52:31 +0200

    find by slug

--- src/main/java/io/ethertale/reasonanddominationspringdefenseproject/dungeon/repo/DungeonRepo.java
+++ src/main/java/io/ethertale/reasonanddominationspringdefenseproject/dungeon/repo/DungeonRepo.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DungeonRepo extends JpaRepository<Dungeon, Integer> {
+    void findDungeonByName(String title);
+    Dungeon findDungeonBySlug(String slug);
 }
