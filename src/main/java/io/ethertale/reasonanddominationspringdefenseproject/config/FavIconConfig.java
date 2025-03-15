9c4cf37b581ced89b99920a52aeb8126aad6e082
Galin Yanakov <galin@ivel.in>
2025-03-15 23:25:31 +0200

    Clean Up

--- src/main/java/io/ethertale/reasonanddominationspringdefenseproject/config/FavIconConfig.java
+++ src/main/java/io/ethertale/reasonanddominationspringdefenseproject/config/FavIconConfig.java
@@ -1,17 +1,8 @@
 package io.ethertale.reasonanddominationspringdefenseproject.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
-import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 @Configuration
 public class FavIconConfig implements WebMvcConfigurer {
