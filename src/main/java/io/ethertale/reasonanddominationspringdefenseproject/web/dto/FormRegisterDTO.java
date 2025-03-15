0d947b8d3cab4e752dc2dcc774dfb295a6c716d5
Galin Yanakov <galin@ivel.in>
2025-03-15 23:25:38 +0200

    Clean Up

--- src/main/java/io/ethertale/reasonanddominationspringdefenseproject/web/dto/FormRegisterDTO.java
+++ src/main/java/io/ethertale/reasonanddominationspringdefenseproject/web/dto/FormRegisterDTO.java
@@ -1,6 +1,8 @@
 package io.ethertale.reasonanddominationspringdefenseproject.web.dto;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
