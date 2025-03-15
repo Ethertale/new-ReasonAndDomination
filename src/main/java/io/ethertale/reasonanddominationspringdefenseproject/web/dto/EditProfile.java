34520f488bb0918048af54e966bdcf46eac41e24
Galin Yanakov <galin@ivel.in>
2025-03-15 23:25:28 +0200

    Clean Up

--- src/main/java/io/ethertale/reasonanddominationspringdefenseproject/web/dto/EditProfile.java
+++ src/main/java/io/ethertale/reasonanddominationspringdefenseproject/web/dto/EditProfile.java
@@ -1,7 +1,6 @@
 package io.ethertale.reasonanddominationspringdefenseproject.web.dto;
 
 import io.ethertale.reasonanddominationspringdefenseproject.account.model.AccountRole;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
