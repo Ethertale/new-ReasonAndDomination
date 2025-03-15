7f93af539701d1a2c8e314bf43132dda3e464535
Galin Yanakov <galin@ivel.in>
2025-03-15 23:25:41 +0200

    Clean Up

--- src/main/java/io/ethertale/reasonanddominationspringdefenseproject/web/controller/ForumController.java
+++ src/main/java/io/ethertale/reasonanddominationspringdefenseproject/web/controller/ForumController.java
@@ -12,7 +12,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Comparator;
 import java.util.UUID;
 
 @Controller
