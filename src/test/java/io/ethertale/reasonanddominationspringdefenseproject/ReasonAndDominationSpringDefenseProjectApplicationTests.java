c26b4e56985cf79eca759b87c12e35b1f1928bb5
Galin Yanakov <galin@ivel.in>
2025-03-18 18:51:16 +0200

    Tests

--- src/test/java/io/ethertale/reasonanddominationspringdefenseproject/ReasonAndDominationSpringDefenseProjectApplicationTests.java
+++ src/test/java/io/ethertale/reasonanddominationspringdefenseproject/ReasonAndDominationSpringDefenseProjectApplicationTests.java
@@ -2,12 +2,8 @@ package io.ethertale.reasonanddominationspringdefenseproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootTest
-@EnableScheduling
-@EnableFeignClients
 class ReasonAndDominationSpringDefenseProjectApplicationTests {
 
     @Test
