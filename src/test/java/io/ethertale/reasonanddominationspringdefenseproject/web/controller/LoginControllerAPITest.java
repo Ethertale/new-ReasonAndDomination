18ae30156ee329a560b7b18c9379649e87591a5f
Galin Yanakov <galin@ivel.in>
2025-03-23 11:10:28 +0200

    API Login Page Test

---
+++ src/test/java/io/ethertale/reasonanddominationspringdefenseproject/web/controller/LoginControllerAPITest.java
@@ -0,0 +1,43 @@
+package io.ethertale.reasonanddominationspringdefenseproject.web.controller;
+
+import io.ethertale.reasonanddominationspringdefenseproject.account.service.ProfileService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(controllers = LoginController.class)
+class LoginControllerAPITest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private ProfileService profileService;
+
+    @Test
+    void getRequestToLoginPage_shouldReturnLoginView() throws Exception {
+
+        MockHttpServletRequestBuilder request = get("/login");
+
+        mockMvc.perform(request)
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(view().name("login"))
+                .andExpect(model().attributeExists("form"));
+    }
+    @Test
+    void getRequestToLoginPageWithErrorParam_shouldReturnLoginViewAndErrorMessageAttribute() throws Exception {
+
+        MockHttpServletRequestBuilder request = get("/login").param("error", "Error Message");
+
+        mockMvc.perform(request)
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(view().name("login"))
+                .andExpect(model().attributeExists("form"));
+    }
+}
