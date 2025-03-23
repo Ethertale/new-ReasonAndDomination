dfba9c68cfdd44b3eb6c394085e47bb158d3316e
Galin Yanakov <galin@ivel.in>
2025-03-23 11:10:35 +0200

    API Register Page Test

---
+++ src/test/java/io/ethertale/reasonanddominationspringdefenseproject/web/controller/RegisterControllerAPITest.java
@@ -0,0 +1,33 @@
+package io.ethertale.reasonanddominationspringdefenseproject.web.controller;
+
+import io.ethertale.reasonanddominationspringdefenseproject.account.service.ProfileService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(controllers = RegisterController.class)
+class RegisterControllerAPITest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private ProfileService profileService;
+
+    @Test
+    void getRequestToRegisterEndPoint_shouldReturnRegisterView() throws Exception {
+
+        MockHttpServletRequestBuilder request = get("/register");
+
+        mockMvc.perform(request)
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(view().name("register"))
+                .andExpect(model().attributeExists("formRegisterDTO"));
+    }
+}
