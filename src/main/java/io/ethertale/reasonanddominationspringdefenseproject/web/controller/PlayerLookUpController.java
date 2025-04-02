9e20abfe7dd31ad2af851833177b8274d0ce564c
Galin Yanakov <galin@ivel.in>
2025-04-02 23:21:31 +0300

    Tests clean up

--- src/main/java/io/ethertale/reasonanddominationspringdefenseproject/web/controller/PlayerLookUpController.java
+++ src/main/java/io/ethertale/reasonanddominationspringdefenseproject/web/controller/PlayerLookUpController.java
@@ -1,17 +1,15 @@
 package io.ethertale.reasonanddominationspringdefenseproject.web.controller;
 
-import io.ethertale.reasonanddominationspringdefenseproject.account.model.Profile;
 import io.ethertale.reasonanddominationspringdefenseproject.account.service.ProfileService;
 import io.ethertale.reasonanddominationspringdefenseproject.web.dto.ProfileLookup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/player-lookup")
