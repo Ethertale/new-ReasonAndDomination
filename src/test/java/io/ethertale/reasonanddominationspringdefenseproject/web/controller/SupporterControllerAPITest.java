d2a4d56537e7fd1f6098661df6cc291ad38326cb
Galin Yanakov <galin@ivel.in>
2025-04-02 19:00:03 +0300

    Support Us API Test

--- src/test/java/io/ethertale/reasonanddominationspringdefenseproject/web/controller/SupporterControllerAPITest.java
+++ src/test/java/io/ethertale/reasonanddominationspringdefenseproject/web/controller/SupporterControllerAPITest.java
@@ -1,6 +1,9 @@
 package io.ethertale.reasonanddominationspringdefenseproject.web.controller;
 
+import io.ethertale.reasonanddominationspringdefenseproject.account.model.AccountRole;
+import io.ethertale.reasonanddominationspringdefenseproject.account.model.Profile;
 import io.ethertale.reasonanddominationspringdefenseproject.account.service.ProfileService;
+import io.ethertale.reasonanddominationspringdefenseproject.security.AuthenticationDetails;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -8,8 +11,11 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
+import java.util.UUID;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -33,8 +39,15 @@ class SupporterControllerAPITest {
     @Test
     void getRequestToSupportUsEndpoint_shouldReturnSupportUsView() throws Exception {
 
-        MockHttpServletRequestBuilder request = get("/support-us");
-        //TODO CHECK WHY 3XX REDIRECT INSTEAD OF 2XX SUCCESSFUL
+        Profile mockProfile = Profile.builder().id(UUID.randomUUID()).role(AccountRole.USER).build();
+
+        when(profileService.getProfileById(any(UUID.class))).thenReturn(mockProfile);
+
+        MockHttpServletRequestBuilder request = get("/support-us")
+                .with(user(new AuthenticationDetails(
+                        UUID.randomUUID(), "user@mail.com", "password", AccountRole.USER, true, "profilePic"
+                ))).with(csrf());
+
         mockMvc.perform(request)
                 .andExpect(status().is2xxSuccessful())
                 .andExpect(view().name("supporterPage"))
